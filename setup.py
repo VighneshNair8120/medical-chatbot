e36ef92ea6ddd527dc196bdfa69bf7d72805e608
Vighnesh Nair <vignu0609@gmail.com>
2025-03-21 19:26:56 +0530

    folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medbot',
+    version= '0.0.0',
+    author= 'Vighnesh Nair',
+    author_email= 'vignu0609@gmail.@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
